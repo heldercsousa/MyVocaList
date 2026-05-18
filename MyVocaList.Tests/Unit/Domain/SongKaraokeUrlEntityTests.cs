5a16165a3f65fe10a173ad765ea3a0d3afc5d017
Helder Carvalho de Sousa <heldercsousa@gmail.com>
2026-05-18 11:52:10 -0300

    feat: add SongKaraokeUrl entity

---
+++ MyVocaList.Tests/Unit/Domain/SongKaraokeUrlEntityTests.cs
@@ -0,0 +1,13 @@
+using MyVocaList.Domain.Entity;
+
+namespace MyVocaList.Tests.Unit.Domain;
+
+public class SongKaraokeUrlEntityTests
+{
+    [Fact]
+    public void SongKaraokeUrl_DefaultPlayCount_IsZero()
+    {
+        var url = new SongKaraokeUrl { VideoId = "dQw4w9WgXcQ", SongId = 1 };
+        Assert.Equal(0, url.PlayCount);
+    }
+}
