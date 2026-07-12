f6c620caac697c0c84acbd30f21f10cd6d194690
Helder Carvalho de Sousa <heldercsousa@gmail.com>
2026-07-11 23:01:39 -0300

    feat: add AutocompleteWindowClass idiom-branch helper for phone autocomplete

---
+++ MyVocaList.Tests/Unit/Components/AutocompleteWindowClassTests.cs
@@ -0,0 +1,48 @@
+using Microsoft.Maui.Devices;
+using MyVocaList.UI.Components.AutocompleteField;
+
+namespace MyVocaList.Tests.Unit.Components;
+
+public class AutocompleteWindowClassTests
+{
+    [Fact]
+    public void IsCompactWindow_PhoneIdiom_ReturnsTrue()
+    {
+        var deviceInfoMock = new Mock<IDeviceInfo>();
+        deviceInfoMock.Setup(d => d.Idiom).Returns(DeviceIdiom.Phone);
+
+        var result = AutocompleteWindowClass.IsCompactWindow(deviceInfoMock.Object);
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsCompactWindow_DesktopIdiom_ReturnsFalse()
+    {
+        var deviceInfoMock = new Mock<IDeviceInfo>();
+        deviceInfoMock.Setup(d => d.Idiom).Returns(DeviceIdiom.Desktop);
+
+        var result = AutocompleteWindowClass.IsCompactWindow(deviceInfoMock.Object);
+
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void IsCompactWindow_TabletIdiom_ReturnsFalse()
+    {
+        var deviceInfoMock = new Mock<IDeviceInfo>();
+        deviceInfoMock.Setup(d => d.Idiom).Returns(DeviceIdiom.Tablet);
+
+        var result = AutocompleteWindowClass.IsCompactWindow(deviceInfoMock.Object);
+
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void IsCompactWindow_NullDeviceInfo_ReturnsFalse()
+    {
+        var result = AutocompleteWindowClass.IsCompactWindow(null);
+
+        Assert.False(result);
+    }
+}
